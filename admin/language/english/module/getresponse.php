a875350a9477a7124c2dd80595136e63b9545864
Karol <kszymanowicz@getresponse.com>
2016-08-05 15:05:48 +0200

    master

---
+++ admin/language/english/module/getresponse.php
@@ -0,0 +1,59 @@
+<?php
+// Heading
+$_['heading_title'] = 'GetResponse';
+$_['text_module'] = 'Modules';
+$_['text_success'] = 'Subscription settings successfully saved.';
+$_['text_connect'] = 'You connected your OpenCart to GetResponse.';
+$_['text_disconnect'] = 'You disconnected your OpenCart from GetResponse.';
+$_['text_export_success'] = 'Export completed.';
+$_['button_export'] = 'Export';
+$_['button_connect'] = "Connect";
+$_['button_disconnect'] = "Disconnect";
+$_['button_stay'] = "Stay connected";
+
+$_['apikey_info'] = 'Enter your GetResponse API key to connect. When you connect, you\'ll have access to your campaigns and forms. All you need to get and export subscribers.';
+$_['export_info'] = '<strong>How it works?</strong><br>This option enables one-time export of contacts from your OpenCart account to GetResponse. Choose a target campaign to which your contacts will be exported. If the campaign includes an autoresponder, choose the sequence day you want to export the contacts to.';
+$_['register_info'] = '<strong>How it works?</strong> <br>You can add subscribers to a selected GetResponse campaign once they register to your online shop via the registration page. Select the target campaign for your OpenCart subscribers. If the campaign includes an autoresponder, choose the sequence day you want to export the contacts to.';
+$_['webform_info'] = '<strong>How to place the form?</strong><br> Simply pick one of the web forms you’ve created at GetResponse and add it to your layout in <strong>Settings > Design > Layouts.</strong>';
+$_['disconnect_info'] = 'This stops new subscribtions via forms, comments, or during account registration.';
+
+$_['apikey_title'] = 'GetResponse Account';
+$_['export_title'] = 'Export customers';
+$_['register_title'] = 'Subscribe via registration form';
+$_['webform_title'] = 'Subscribe via a form';
+$_['disconnect_title'] = 'Are you sure you want to disconnect from GetResponse?';
+
+$_['label_active'] = 'Active:';
+$_['label_active_register'] = 'Allow subscriptions when visitors register';
+$_['label_active_forms'] = 'Allow subscriptions via forms';
+$_['label_form'] = 'Select the form';
+$_['label_campaign'] = 'Select the campaign';
+$_['label_day_of_cycle'] = 'Select the autoresponder';
+$_['label_auto_queue'] = 'Add to autoresponder sequence';
+$_['label_yes'] = 'Yes';
+$_['label_no'] = 'No';
+$_['label_none'] = '--- none ---';
+$_['label_new_forms'] = 'New forms';
+$_['label_old_forms'] = 'Old forms';
+$_['label_export_info'] = 'Need help with export?';
+$_['label_register_info'] = 'Need help with registrations?';
+$_['label_webform_info'] = 'Need help with forms?';
+
+
+$_['info_loading'] = 'Loading...';
+$_['info_ajax_error'] = 'Ajax request error.';
+
+$_['entry_export'] = '<span class="help"></span>';
+$_['entry_enable_module'] = 'Module:';
+$_['entry_apikey'] = 'API key';
+$_['entry_apikey_hint'] = 'You can find your API key in <a href="https://app.getresponse.com/manage_api.html" target="_blank">integration settings</a> of your GetResponse account. Log in to GetResponse and go to My account > API & OAuth to find the key.';
+$_['entry_campaign_hint'] = 'The list is automatically grabbed from your GetResponse account.';
+$_['entry_campaign_description'] = 'The list is automatically grabbed from your GetResponse account.';
+$_['entry_register_integration'] = 'Register Account integration:<span class="help"></span>';
+$_['entry_guest_integration'] = 'Guest Checkout integration:<span class="help"></span>';
+
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify module GetResponse!';
+$_['error_apikey'] = 'Apikey required!';
+$_['error_incorrect_apikey'] = 'The API key seems incorrect. Please check if you typed or pasted it correctly. If you recently generated a new key, please make sure you’re using the right one.';
